660bcb933ab61de1bc4e3e3c6de1d6f1f8afe1a7
DiegoOLopez <dieobelopcas01@gmail.com>
2022-12-26 05:19:46 -0600

    Charts

---
+++ charts/charts.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def generatePieChart():
+    labels = ['A', 'B', 'C']
+    values = [200, 34, 340]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('pie.png')
+    plt.close()
+
+generatePieChart()

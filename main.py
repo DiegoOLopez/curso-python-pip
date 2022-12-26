e9672c02031c026b824c54e1377006aa282df3ad
DiegoOLopez <dieobelopcas01@gmail.com>
2022-12-26 04:58:45 -0600

    Tercer cambio

--- main.py
+++ main.py
@@ -1,16 +1,85 @@
-# This is a sample Python script.
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def choiceOptions():
+    options = ('piedra', 'papel', 'tijera')
+    user_option = input('piedra, papel o tijera => ')
+    user_option = user_option.lower()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    if not user_option in options:
+        print('esa opcion no es valida')
+        #continue
+        return None, None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    computer_option = random.choice(options)
+
+    print('User option =>', user_option)
+    print('Computer option =>', computer_option)
+    return user_option, computer_option
+
+def checkRules(user_option, computer_option, user_wins, computer_wins):
+
+    if user_option == computer_option:
+        print('Empate!')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('piedra gana a tijera')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('Papel gana a piedra')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print('papel gana a piedra')
+            print('user gano')
+            user_wins += 1
+        else:
+            print('tijera gana a papel')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('tijera gana a papel')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('piedra gana a tijera')
+            print('computer gano!')
+            computer_wins += 1
+    return user_wins, computer_wins
+
+def checkWinner(user_wins, computer_wins):
+    if computer_wins == 2:
+        print('El ganador es la computadora')
+        exit()
+
+    if user_wins == 2:
+        print('El ganador es el usuario')
+        exit()
+
+
+def runGame():
+    computer_wins = 0
+    user_wins = 0
+    rounds = 1
+    while True:
+        user_option, computer_option = choiceOptions()
+        print('*' * 10)
+        print('ROUND', rounds)
+        print('*' * 10)
+
+        print('computer_wins', computer_wins)
+        print('user_wins', user_wins)
+
+        rounds += 1
+
+        user_wins, computer_wins = checkRules(user_option, computer_option, user_wins, computer_wins)
+
+        checkWinner(user_wins, computer_wins)
+
+
+
+
+runGame()
